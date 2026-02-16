6d92b1a259e0016ffd2ff84dcb75c23e93c47314
CODERSR77 <rsrthecoder3333333@gmail.com>
2026-02-16 18:32:28 +0530

    Updated the main.py file

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2026  <Your Name>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+
 from pathlib import Path
 import os
 from click import confirm
